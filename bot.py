e1877d922f2650a3ef5d78a39e248559123e3787
wangtim1996 <wangtim1996@users.noreply.github.com>
2021-12-21 18:26:47 -0800

    webhook test

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ async def on_ready():
 
 @bot.command(name='version')
 async def versionCheck(context):
-    await context.send("0.11")
+    await context.send("0.12")
 
 @bot.command(name='bicolor')
 async def bicolor(context):
